1677b21f6b3fcb438a0351fde1d730bb31858d9c
nichoth <nichoth@gmail.com>
2015-10-06 10:59:34 -0700

    router works

--- browser/main.js
+++ browser/main.js
@@ -1,25 +1,9 @@
 var vdom = require('virtual-dom');
 var mainLoop = require('main-loop');
 var url = require('url');
-var catchLinks = require('catch-links');
-var singlePage = require('single-page');
-
-var EventEmitter = require('events').EventEmitter;
-var bus = new EventEmitter();
-var emit = bus.emit.bind(bus);
 
 var App = require('../lib/App.js');
-var router = require('../lib/routes')(emit);
-
-var show = singlePage(function(href) {
-  var path = url.parse(href).pathname;
-  var m = router.match(path);
-  if (m) {
-    m.fn();
-  }
-});
-
-catchLinks( document.getElementById('content'), show.bind(null) );
+// var router = require('../lib/routes')();
 
 var items = {
   videos: {
@@ -36,13 +20,17 @@ var items = {
   }
 };
 
-bus.on('route', function(state) {
-  state.items = items;
-  console.log(state);
-  loop.update( App(state) );
-});
+// bus.on('route', function(state) {
+//   state.items = items;
+//   console.log(state);
+//   loop.update( App(state) );
+// });
 
-var loop = mainLoop(App(), App.render, vdom);
+var app = App({items: items});
+
+var loop = mainLoop(app(), App.render, vdom);
 document.getElementById('content').appendChild(loop.target);
 
-emit('route', {});
+app(loop.update);
+
+// emit('route', {});
