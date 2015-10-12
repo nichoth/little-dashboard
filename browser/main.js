13e2420a68a156f1c6bc7372a90e980415f187ab
nichoth <nichoth@gmail.com>
2015-10-12 14:25:15 -0700

    router refactor

--- browser/main.js
+++ browser/main.js
@@ -1,6 +1,8 @@
 var vdom = require('virtual-dom');
 var mainLoop = require('main-loop');
+var state = require('@nichoth/state');
 
+var router = require('../lib/routes')( document.getElementById('content') );
 var App = require('../lib/App.js');
 
 var items = {
@@ -20,7 +22,34 @@ var items = {
 
 var app = App({items: items});
 
-var loop = mainLoop(app(), App.render, vdom);
+
+var s = state({
+  app: App({
+    items: items,
+  })
+});
+
+router.on('route', function(data) {
+
+  var activeItem = data.activeItem;
+  var renderFn = data.renderFn;
+  var page = data.page;
+
+  s.app.set( App({
+    items: items,
+    activeItem: activeItem,
+    renderFn: renderFn,
+    page: page
+  })() );
+
+});
+
+
+var loop = mainLoop(s(), render, vdom);
 document.getElementById('content').appendChild(loop.target);
 
-app(loop.update);
+s(loop.update);
+
+function render(state) {
+  return App.render(state.app);
+}
