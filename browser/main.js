b84bfb5072fb0df90adf9b7a06c02610fdf49cfd
nichoth <nichoth@gmail.com>
2015-10-15 16:38:32 -0700

    trying things

--- browser/main.js
+++ browser/main.js
@@ -1,8 +1,8 @@
 var vdom = require('virtual-dom');
 var mainLoop = require('main-loop');
-var state = require('@nichoth/state');
+// var state = require('@nichoth/state');
 
-var router = require('../lib/routes')( document.getElementById('content') );
+// var router = require('../lib/routes')( document.getElementById('content') );
 var App = require('../lib/App.js');
 
 var items = {
@@ -20,35 +20,36 @@ var items = {
   }
 };
 
-var app = App({items: items});
+// var app = App({items: items});
 
 
-var s = state({
-  app: App({
-    items: items,
-  })
-});
+// var s = state({
+//   app: App({
+//     items: items,
+//   })
+// });
 
-router.on('route', function(data) {
-  var activeItem = data.activeItem;
-  var renderFn = data.renderFn;
-  var page = data.page;
+// router.on('route', function(data) {
+//   var activeItem = data.activeItem;
+//   var renderFn = data.renderFn;
+//   var page = data.page;
 
-  s.app.set( App({
-      items: items,
-      activeItem: activeItem,
-      renderFn: renderFn,
-      page: page
-    })()
-  );
-});
+//   s.app.set( App({
+//       items: items,
+//       activeItem: activeItem,
+//       renderFn: renderFn,
+//       page: page
+//     })
+//   );
+// });
 
+var app = App();
 
-var loop = mainLoop(s(), render, vdom);
+var loop = mainLoop(app(), App.render, vdom);
 document.getElementById('content').appendChild(loop.target);
 
-s(loop.update);
+app(loop.update);
 
-function render(state) {
-  return App.render(state.app);
-}
+// function render(state) {
+//   return App.render(state);
+// }
