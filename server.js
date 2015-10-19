aa12e40ebcb0072480206fcb0eff8c0ef1e45cea
nichoth <nichoth@gmail.com>
2015-10-19 09:40:20 -0700

    trying level

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var ecstatic = require('ecstatic')({root: __dirname + '/public'});
 var http = require('http');
 var fs = require('fs');
 var router = require('routes')();
+var foreach = require('lodash.foreach');
+
+var wsock = require('websocket-stream');
+var multilevel = require('multilevel');
+
+var db = require('./data/db.js');
 
 router.addRoute('/api/:name', function(req, res, params) {
   res.setHeader("Content-Type", "text/html");
@@ -14,12 +20,12 @@ function appRoute(req, res) {
     .pipe(res);
 }
 
-router.addRoute('/videos', appRoute);
-router.addRoute('/videos/new', appRoute);
-router.addRoute('/articles', appRoute);
-router.addRoute('/photos', appRoute);
+// serve static app at all routes
+foreach( require('./lib/route-hash.js'), function(fn, routeStr) {
+  router.addRoute(routeStr, appRoute);
+});
 
-http.createServer(function(req, res) {
+var server = http.createServer(function(req, res) {
 
   var m = router.match(req.url);
   if (m) {
@@ -31,3 +37,61 @@ http.createServer(function(req, res) {
 
 }).listen(8000);
 console.log('listening on :8000');
+
+
+
+wsock.createServer({ server: server }, function(stream) {
+  stream.pipe( multilevel.server(db) ).pipe(stream);
+});
+
+
+
+
+
+
+// var level = require('level');
+// var multilevel = require('multilevel');
+// var http = require('http');
+// var browserify = require('browserify');
+// var shoe = require('shoe');
+
+// // A read stream ends when it reaches the end of the key range.
+// // `level-live-stream` makes a stream that stays open and emits events
+// // whenever the db changes.
+// var liveStream = require('level-live-stream');
+
+
+// // server
+// var server = http.createServer(function(req, res) {
+//   if (req.url == '/') {
+//     res.end('<script type="text/javascript" src="/bundle.js"></script>');
+//   } else if (req.url == '/bundle.js') {
+//     browserify(__dirname + '/app.js', { debug:true })
+//       .bundle()
+//       .pipe(res);
+//   } else {
+//     res.end('nope');
+//   }
+// });
+
+// server.listen(8000);
+// console.log('listening on port 8000');
+
+// // db
+// var db = level(__dirname + '/db', {
+//   valueEncoding: 'json',
+// });
+
+// // this lets multilevel clients use `db.liveStream()` via the manifest
+// liveStream.install(db);
+
+// // the manifest is necessary for the client (browser-side multilevel) to use
+// // our level plugins.
+// multilevel.writeManifest(db, __dirname+'/manifest.json');
+
+// // websockets
+// var sock = shoe(function(stream) {
+//   stream.pipe(multilevel.server(db)).pipe(stream);
+// });
+
+// sock.install(server, '/sock');
