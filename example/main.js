3747c0cf8e59e34a1b866d47b9c3e1bfe5a5bdcd
nichoth <nichoth@gmail.com>
2015-11-03 11:12:40 -0800

    factor out schema

--- example/main.js
+++ example/main.js
@@ -1,4 +1,6 @@
 var dashboard = require('../');
 
-dashboard( document.getElementById('content') );
+dashboard(document.getElementById('content'), {
+  schema: require('./schema.js')
+});
 
---
+++ example/schema.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    itemName: 'video',
+    itemNamePlural: 'videos',
+    fields: {
+      title: 'string',
+      url: 'string',
+      author: 'string',
+      date: 'date'
+    }
+  },
+  {
+    itemName: 'article',
+    itemNamePlural: 'articles',
+    fields: {
+      title: 'string',
+      author: 'string',
+      date: 'date'
+    }
+  },
+  {
+    itemName: 'photo',
+    itemNamePlural: 'photos',
+    fields: {
+      title: 'string',
+      author: 'string',
+      date: 'date'
+    }
+  }
+];
